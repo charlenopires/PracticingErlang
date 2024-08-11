262c0a296e87f26715c78c71b3e9997615998a2c
Charleno Pires <charlenopires@ifpi.edu.br>
2024-08-11 14:47:37 -0300

    Updating Math

--- basic/math.erl
+++ basic/math.erl
@@ -6,7 +6,28 @@ add(X, Y, Z) -> X + Y + Z.
 fac(0) -> 1;
 fac(N) -> N * fac(N-1).
 
+sin(X) -> math:sin(X).
+cos(X) -> math:cos(X).
+tan(X) -> math:tan(X).
+mean(list) when is_list(list) -> lists:sum(list) / length(list).
+
+median(List) when is_list(List) ->
+    Sorted = lists:sort(List),
+    Len = length(Sorted),
+    if
+        Len rem 2 == 1 -> lists:nth((Len div 2) + 1, Sorted);
+        true -> (lists:nth(Len div 2, Sorted) + lists:nth((Len div 2) + 1, Sorted)) / 2
+    end.
 init() ->
-    io:format("math module loaded~n"),
+    List = [1,2,3,4,5,6,7,8,9,10],
+    SumList = lists:sum(List),
+    io:format("Math Module Loaded~n"),
     io:format("~n~p",add(34, 56)),
+    io:format("Sine: sin(pi/2) = ~p~n", [sin(math:pi() / 2)]),
+    io:format("Cosine: cos(0) = ~p~n", [cos(0)]),
+    io:format("Tangent: tan(pi/4) = ~p~n", [tan(math:pi() / 4)]),
+    io:format("Factorial: fac(5) = ~p~n", [fac(5)]),
+    io:format("Sum of the numbers in the list: ~p~n", SumList),
+    io:format("Média: mean([1, 2, 3, 4, 5]) = ~p~n", [mean([1, 2, 3, 4, 5])]),
+    io:format("Mediana: median([1, 2, 3, 4, 5]) = ~p~n", [median([1, 2, 3, 4, 5])]),
     ok.
